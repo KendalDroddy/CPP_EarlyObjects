cc2f0b5ca354030707498360f439771267aed11d
Kendal Droddy <kdroddy001@gmail.com>
2017-02-04 18:16:07 -0600

    first commit

---
+++ Chapter_2/2_1.cpp
@@ -0,0 +1,21 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: February 4, 2017
+**
+** Challenge: Write a program that stores the integers 50 and 100 in 
+** variables and stores the sum of these two in a variable named 
+** total. Display the total on the screen.
+*********************************************************************/
+#include <iostream>
+
+int main()
+{
+	int a = 50,
+		b = 100,
+		total;
+
+	//Calculate and display total
+	total = a + b;
+	std::cout << total << std::endl;
+}
