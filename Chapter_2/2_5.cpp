9b509dbcd7e962f7631579efea78031b03c79150
Kendal Droddy <kdroddy001@gmail.com>
2017-02-06 09:38:24 -0600

    first commit

---
+++ Chapter_2/2_5.cpp
@@ -0,0 +1,21 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: February 6, 2017
+**
+** Challenge: A car holds 16 gallons of gasoline and can travel 312
+** miles before refueling. Write a program that calculates the number
+** of miles per gallon the car gets. Display the results on scree.
+*********************************************************************/
+#include <iostream>
+
+int main()
+{
+  //Initialize and define variables
+  double gallons = 16,
+         miles = 312,
+         milesPerGallon = miles/gallons;
+
+  //Display results
+  std::cout << "Miles per gallon: " << milesPerGallon << std::endl;
+}
