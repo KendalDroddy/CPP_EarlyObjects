61eadea87c8237e1d84417e39981f4524fb2bcfa
Kendal Droddy <kdroddy001@gmail.com>
2017-02-06 13:52:16 -0600

    first commit

---
+++ Chapter_2/2_11.cpp
@@ -0,0 +1,24 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: February 6, 2017
+**
+** Challenge: Write a program that displays the following pattern
+** on the screen:  *
+**                * * 
+**               * * *
+**              * * * *
+**             * * * * *
+*********************************************************************/
+#include <iostream>
+using std::cout;
+
+int main()
+{
+  cout << "\n";
+  cout << "    * \n";
+  cout << "   * * \n";
+  cout << "  * * * \n";
+  cout << " * * * * \n";
+  cout << "* * * * *" << std::endl;
+}
