9b50d90db6c1962d7e9513c0117e90306b0d5db8
Kendal Droddy <kdroddy001@gmail.com>
2017-02-06 13:45:43 -0600

    first commit

---
+++ Chapter_2/2_10.cpp
@@ -0,0 +1,15 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: February 6, 2017
+**
+** Challenge: Write a program that displays the following information
+** each on a separate line: Name, Address, Phone, College major.
+** Use only a single cout statement to display the information.
+*********************************************************************/
+#include <iostream>
+
+int main()
+{
+  std::cout << "Name: Kendal\n" << "Address: 312 Foo Bar Road\n" << "Phone: 888-888-8888\n" << "College Major: Computer Science" << std::endl;
+}
