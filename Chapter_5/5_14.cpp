448d49deb7efe5e3583dde71aa42d9c2fbb7f6ac
Kendal Droddy <kdroddy001@gmail.com>
2017-02-23 20:52:11 -0600

    first commit

---
+++ Chapter_5/5_14.cpp
@@ -0,0 +1,16 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: February 23, 2017
+**
+** Challenge: Write a program with a loop that lets the user enter a 
+** series of integers, followed by -99 to signal the end of the 
+** series. After all the numbers have been entered, the program 
+** should display the largest and smallest numbers entered .
+*********************************************************************/
+#include <iostream>
+
+int main()
+{
+  
+}
