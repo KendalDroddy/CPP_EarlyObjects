a2d13539e094f12f2b7cadf4751715ea1e41be9c
Kendal Droddy <kdroddy001@gmail.com>
2017-02-23 21:13:51 -0600

    first commit

--- Chapter_5/5_14.cpp
+++ Chapter_5/5_14.cpp
@@ -12,5 +12,38 @@
 
 int main()
 {
-  
+  //Declare variables
+ 	int value,
+ 			maxInteger,
+ 			minInteger;
+
+ 	//Get user input
+ 	std::cout << "\nEnter a series of integers followed by\n";
+ 	std::cout << "-99 when you're done. This program will\n";
+ 	std::cout << "return the largest and smallest value entered.\n" << std::endl;
+
+ 	//Get input to initialize min and max values
+ 	std::cout << "Enter an integer: ";
+ 	std::cin >> value;
+ 	maxInteger = value;
+ 	minInteger = value;
+
+ 	//User input loop
+ 	while(value != -99)
+ 	{
+ 		std::cout << "Enter an integer: ";
+ 		std::cin >> value;
+ 		if (value < minInteger)
+ 		{
+ 			minInteger = value;	
+ 		}
+ 		if (value > maxInteger)
+ 		{
+ 			maxInteger = value;
+ 		}
+ 	}
+
+ 	//Display results
+ 	std::cout << "Max value: " << maxInteger << std::endl;
+ 	std::cout << "Min value: " << minInteger << std::endl;
 }
