13c92f191bb1631ab47a3c9e6ff5f2d945fcfcb8
Kendal Droddy <kdroddy001@gmail.com>
2017-03-11 11:41:34 -0600

    first commit

---
+++ Chapter_5/5_28.cpp
@@ -0,0 +1,52 @@
+/*********************************************************************
+** Author: Kendal Droddy
+**
+** Date: March 11, 2017
+**
+** Description: Modify program 5_15 so that it gets the names from 
+** a data file. Names should be read in until there is no more data
+** to read.
+*********************************************************************/
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+  //Declare variables
+  std::string student,
+              firstStudent,
+              lastStudent;
+  int numStudents;
+  std::ifstream inputFile;
+
+  //Open input file and check for errors
+  inputFile.open("LineUp.txt");
+
+  if (inputFile.fail())
+  {
+    std::cout << "Error opening file." << std::endl;
+  }
+  else
+  {
+    //Initialize first and last student with first name from input
+    inputFile >> student;
+    firstStudent = student;
+    lastStudent = student;
+
+    //Process the remaining input
+    while(inputFile >> student)
+    {
+      if (student > lastStudent)
+      {
+        lastStudent = student;
+      }
+      if (student < firstStudent)
+      {
+        firstStudent = student;
+      }
+    }
+  }
+  //Display results
+  std::cout << "\nFirst student in line: " << firstStudent << std::endl;
+  std::cout << "Last student in line: " << lastStudent << std::endl;
+}
